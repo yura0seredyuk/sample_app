14ce3441ad1447a25098398cd58945100e3ac55b
yura0seredyuk <yura.seredyuk0@gmail.com>
2020-10-14 00:16:39 +0300

    Finish layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
 
-  get 'static_pages/home'
+  get 'home' => 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
+
+  get 'contact' => 'static_pages#contact'
+
+  get 'signup' => 'users#new'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get "/signup"
+    assert_response :success
+  end
+
+end
